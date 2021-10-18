7d0852abdeeedcd17c91fbfde61e56a6677b4079
DavideBernardini <84407164+DavideBernardini@users.noreply.github.com>
2021-10-18 18:03:36 +0200

    Aggiustata badWord e lunghezza testo con censura

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
     È una produzione tipica siciliana, come tale è stata ufficialmente inserita nella lista dei prodotti agroalimentari tradizionali italiani (P.A.T) del Ministero delle politiche agricole alimentari e forestali (Mipaaf).
     L'elioconcentrato è prodotto nelle province di Agrigento, Messina, Palermo, Ragusa e Trapani. Il primo passo della produzione consiste nella bollitura dei pomodori al fine di restringere il sugo. Ottenuta la passata di pomodoro, essa viene setacciata, e posta su ripiani di legno o piatti e vassoi di maiolica chiamati sperlonghe. La passata adagiata sui ripiani viene lasciata quindi sotto il sole coperta solo da un velo di tessuto per evitare che gli insetti vi finiscano sopra. Nelle preparazioni casalinghe, l'elioconcentrato viene messo tradizionalmente in vasi di terracotta, o più spesso in vasi di vetro. Il contenuto infine viene ricoperto con una foglia d'alloro e, prima di tappare, sigillato con uno strato d'olio.
     È un processo di concentrazione del sugo di pomodoro totalmente naturale che sfrutta il clima assolato della Sicilia. Durante il processo di essiccazione il sugo viene più volte girato e mescolato. Viene aggiunto il sale secondo necessità. Il processo di essiccazione assolutamente naturale e non aggressivo nei tempi e nelle temperature permette al prodotto di giungere ad un livello di qualità non raggiungibile dalle produzioni industriali. Prima dell'uso, come tutti i concentrati di pomodoro, deve essere diluito in acqua. ";
-    $badWord = strtolower($_GET['censura']);
-    $textCensored = str_replace($badWord, '<strong>***</strong>', $text);
+    $badWordLower = strtolower($_GET['censura']);
+    $badWordUpper = strtoupper($_GET['censura']);
+    $badWordCapitalize = ucwords($_GET['censura']);
+    $textCensored = str_replace(array($badWordLower, $badWordCapitalize, $badWordUpper), '<strong style="background-color: red; vertical-align: middle">***</strong>', $text);
+    $textCensoredLeangth = strlen(str_replace('<strong style="background-color: red">***</strong>', '', $textCensored));
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,16 +22,16 @@
     <p>
         <?php echo $text; ?>
     </p>
-    <hr>
     <div>
         <strong>Lunghezza testo:</strong> <?php echo strlen($text); ?>
     </div>
+    <hr>
     <h2>Elioconcentrato Censurato</h2>
     <p>
         <?php echo $textCensored; ?>
     </p>
     <div>
-        <strong>Lunghezza testo:</strong> <?php echo strlen($textCensored); ?>
+        <strong>Lunghezza testo:</strong> <?php echo $textCensoredLeangth; ?>
     </div>
 </body>
 </html>
