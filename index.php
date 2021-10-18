f71742531167d9199ffabbf319835cc52679afca
DavideBernardini <84407164+DavideBernardini@users.noreply.github.com>
2021-10-18 17:33:33 +0200

    modifica

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     È una produzione tipica siciliana, come tale è stata ufficialmente inserita nella lista dei prodotti agroalimentari tradizionali italiani (P.A.T) del Ministero delle politiche agricole alimentari e forestali (Mipaaf).
     L'elioconcentrato è prodotto nelle province di Agrigento, Messina, Palermo, Ragusa e Trapani. Il primo passo della produzione consiste nella bollitura dei pomodori al fine di restringere il sugo. Ottenuta la passata di pomodoro, essa viene setacciata, e posta su ripiani di legno o piatti e vassoi di maiolica chiamati sperlonghe. La passata adagiata sui ripiani viene lasciata quindi sotto il sole coperta solo da un velo di tessuto per evitare che gli insetti vi finiscano sopra. Nelle preparazioni casalinghe, l'elioconcentrato viene messo tradizionalmente in vasi di terracotta, o più spesso in vasi di vetro. Il contenuto infine viene ricoperto con una foglia d'alloro e, prima di tappare, sigillato con uno strato d'olio.
     È un processo di concentrazione del sugo di pomodoro totalmente naturale che sfrutta il clima assolato della Sicilia. Durante il processo di essiccazione il sugo viene più volte girato e mescolato. Viene aggiunto il sale secondo necessità. Il processo di essiccazione assolutamente naturale e non aggressivo nei tempi e nelle temperature permette al prodotto di giungere ad un livello di qualità non raggiungibile dalle produzioni industriali. Prima dell'uso, come tutti i concentrati di pomodoro, deve essere diluito in acqua. ";
-    $badWord = $_GET['censura'];
+    $badWord = strtolower($_GET['censura']);
     $textCensored = str_replace($badWord, '<strong>***</strong>', $text);
 ?>
 <!DOCTYPE html>
@@ -27,5 +27,8 @@
     <p>
         <?php echo $textCensored; ?>
     </p>
+    <div>
+        <strong>Lunghezza testo:</strong> <?php echo strlen($textCensored); ?>
+    </div>
 </body>
 </html>
